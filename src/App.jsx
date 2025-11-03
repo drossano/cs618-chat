858505623f21c808f2f6e19f9c2eb7158446bac7
drossano <deanrossano+dev@gmail.com>
2025-11-03 12:33:40 -0500

    remove io import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { io } from 'socket.io-client'
 import { Blog } from './pages/Blog.jsx'
 import { Signup } from './pages/Signup.jsx'
 import { Login } from './pages/Login.jsx'
