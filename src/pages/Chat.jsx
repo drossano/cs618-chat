2abc2ab16b0b95e84e826ac909cf9f8f48d39f7b
drossano <deanrossano+dev@gmail.com>
2025-11-03 19:17:05 -0500

    feat: implement chatroom in frontend

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,10 @@
 import { Header } from '../components/Header.jsx'
 import { Status } from '../components/Status.jsx'
+import { ChatRoom } from '../components/ChatRoom.jsx'
+import { useSocket } from '../contexts/SocketIOContext.jsx'
 
 export function Chat() {
+  const { status } = useSocket()
   return (
     <div style={{ padding: 8 }}>
       <Header />
@@ -9,6 +12,10 @@ export function Chat() {
       <hr />
       <br />
       <Status />
+      <br />
+      <hr />
+      <br />
+      {status === 'connected' && <ChatRoom />}
     </div>
   )
 }
